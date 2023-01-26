455960725f805353513b291df426a1fa0391d397
Luwengier <g217881111@gmail.com>
2023-01-26 23:06:06 +0800

    Change app into variables and mutability demo

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,12 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("請猜測一個數字！");
+    let x = 5;
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    loop {
-        println!("請輸入你的猜測數字。");
+    let x = x + 1;
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("讀取該行失敗");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("你的猜測數字：{guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("太小了！"),
-            Ordering::Greater => println!("太大了！"),
-            Ordering::Equal => {
-                println!("獲勝！");
-                break;
-            }
-        }
+    {
+        let x = x * 2;
+        println!("x 在內部範圍的數值為：{x}");
     }
+
+    println!("x 的數值為：{x}");
 }
